fc4e4d7f602127def89a0826decb3d8fbf047b2d
Izumi Konata <sasawat@outlook.com>
2015-02-16 15:51:30 -0500

    branched from master

---
+++ ccs_projects/maav/Debug/qc_test.pp
@@ -0,0 +1,79 @@
+# FIXED
+
+qc_test.obj: ../qc_test.c
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_i2c.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c123gh6pm.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/i2c.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/eeprom.h
+qc_test.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/PPM.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/px4_i2c.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/servoIn.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/time_util.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/px4_kalman.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/dof.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/math.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/ymath.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/quad_ctrl.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/math.h
+qc_test.obj: C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/utility.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h
+qc_test.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h
+
+../qc_test.c: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c123gh6pm.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/i2c.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/eeprom.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/utils/uartstdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/PPM.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/px4_i2c.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/servoIn.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/time_util.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/px4_kalman.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/dof.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/math.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/ymath.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/quad_ctrl.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/math.h: 
+C:/Users/Sasawat/2workspace/maav-controls/ccs_projects/maav/include/utility.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdlib.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdio.h: 
