37d36224b2d9e4257856cee90e0dceb98a17c5a0
Sajan Patel <sajanptl@umich.edu>
2015-06-23 00:13:38 -0400

    adding px4's i2c_frame struct as px4Frame in Px4Defines.hpp so that we can get sizeof() it and use it in parsing
    Also modified Px4 class to incorporate all testing, but it still needs to be run through unit tests

--- include/Px4.hpp
+++ include/Px4.hpp
@@ -2,27 +2,41 @@
 #define PX4_HPP_
 
 #include <stdint.h>
+#include "Px4Defines.hpp"
 
-class Px4 {
-private:
-	uint16_t _frameCount;
-	int16_t _pixelFlowXSum; // accumulated x flow in pixels * 10 since last I2C frame
-	int16_t _pixelFlowYSum; // accumulated y flow in pixels * 10 since last I2C frame
-	int16_t _flowCompMX; // x velocity * 1000 in meters / timestep
-	int16_t _flowCopmMY; // y velocity * 1000 in meters / timestep
-	int16_t _qual; // optical flow quality / confidence 0: bad, 255: maximum quality
-	int16_t _gyroXRate;
-	int16_t _gyroYRate;
-	int16_t _gyroZRate;
-	uint8_t _gyroRange;
-	uint8_t _sonarTimestamp;
-	int16_t _groundDistance;
+// Union for raw array and px4 message frame
+typedef union _px4FrameBuffer
+{
+	px4Frame data;
+	uint8_t raw[sizeof(px4Frame)];
+} px4FrameBuffer;
+
+
+// Class representation of PX4
+class Px4
+{
 public:
 	Px4();
+	
+	// parsing function
+	void parse(uint8_t *raw);
 
-	~Px4();
+	// getters for data that also apply appropriate scaling
+	uint16_t getFrameCount() const;		// returns the i2c framecount
+	uint32_t getSonarTimestamp() const;	// returns timestamp in usec
+	uint8_t getGyroRange() const; 		// scaled appropraitely
+	float getPixelXFlow() const;		// in pixels/s
+   	float getPixelYFlow() const;		// in pixels/s
+	float getXFlow() const;				// in m/s
+	float getYFlow() const;				// in m/s
+	float getQual() const;				// a percentage of how good flow is
+	float getGyroXRate() const;			// in rad/s
+	float getGyroYRate() const;			// in rad/s
+	float getGyroZRate() const;			// in rad/s
+	float getZDist() const;				// ground distance in meters
 
-	uint16_t frameCount() const;
+private:
+	px4FrameBuffer fb; // frame buffer of PX4 message that is being parsed
 };
 
 #endif /* PX4_HPP_ */
--- src/Px4.cpp
+++ src/Px4.cpp
@@ -1,3 +1,76 @@
+#include <cstdlib>
+#include <stdint.h>
 #include "Px4.hpp"
+#include "Px4Defines.hpp"
 
+Px4::Px4()
+{
+	for (int i = 0; i < sizeof(px4Frame); ++i) fb.raw[i] = 0;
+}
 
+void Px4::Parse(const uint8_t *raw)
+{
+	for (int i = 0; i < sizeof(px4Frame); ++i) fb.raw[i] = raw[i];
+}
+
+uint16_t Px4::getFrameCount() const
+{
+	return fb.data.frame_count
+}
+
+uint32_t Px4::getSonarTimestamp() const
+{
+	return 1000 * (uint32_t)fb.data.sonar_timestamp;
+}
+
+uint8_t Px4::getGyroRange() const
+{
+	return fb.data.gyro_range;
+}
+
+float Px4::getPixelXFlow() const
+{
+	return (float)fb.data.pixel_flow_x_sum / 10.0;
+}
+
+float Px4::getPixelYFlow() const
+{
+	return (float)fb.data.pixel_flow_y_sum / 10.0;
+}
+
+float Px4::getXFlow() const
+{
+	return (float)fb.data.flow_comp_m_x / 1000.0;
+}
+
+float Px4::getYFlow() const
+{
+	return (float)fb.data.flow_comp_m_y / 1000.0;
+}
+
+float Px4::getQual() const
+{
+	return (float)fb.data.qual / 255.0;
+}
+
+float Px4::getGyroXRate() const
+{
+	return (float)fb.data.gyro_x_rate;
+}
+
+float Px4::getGyroYRate() const
+{
+	return (float)fb.data.gyro_y_rate;
+}
+
+float Px4::getGyroZRate() const
+{
+	return (float)fb.data.gyro_z_rate;
+}
+
+float Px4::getZDist() const
+{
+	return (float)fb.data.ground_dist / 1000.0;
+}
+
+// End of File
