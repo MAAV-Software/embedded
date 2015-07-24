2db3701bb13a44d73a0194ad60078f08039127a5
Sajan Patel <sajanptl@umich.edu>
2015-07-24 02:25:21 -0400

    Added Greg's Battery class
    Added Greg's battery monitoring code with some changes.
    Battery class monitors the battery voltage and provides service for calculating
    voltage from raw adc codes and determining if the voltage is too low based on
    per-cell-voltage and a minimum threshold. The files for this are
    include/Battery.hpp and src/Battery.cpp.
    
    Also added test case in test/BatteryTest.cpp and updated Makefile to build
    the boost test cases for it. Ran test cases and they all pass.

---
+++ test/BatteryTest.cpp
@@ -0,0 +1,37 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Battery Test"
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include <stdint.h>
+#include "Battery.hpp"
+
+struct Fixture
+{
+	Fixture()
+	{
+		b2 = Battery(16, 4, 1);
+	}
+
+	Battery b1, b2;
+};
+
+BOOST_AUTO_TEST_CASE(ctorTest)
+{
+	Fixture f;
+	BOOST_CHECK_CLOSE(f.b1.getVolts(), 14.8f, 0.001);
+	BOOST_CHECK_CLOSE(f.b2.getVolts(), 16, 0.001);
+	BOOST_CHECK(!f.b1.isLow());
+	BOOST_CHECK(!f.b2.isLow());
+}
+
+BOOST_AUTO_TEST_CASE(updateTest)
+{
+	Fixture f;
+	f.b1.update(12, 1);
+	f.b2.update(4, 1);
+	BOOST_CHECK_CLOSE(f.b1.getVolts(), 12, 0.001);
+	BOOST_CHECK_CLOSE(f.b2.getVolts(), 4, 0.001);
+	BOOST_CHECK(f.b1.isLow());
+	BOOST_CHECK(f.b2.isLow());
+}
