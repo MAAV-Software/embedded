08154e32dcae5b49462588c065ded6bd56be8c32
Steven Schulte <spschul@umich.edu>
2016-02-06 21:14:40 -0500

    Intermediate update with questions

--- src/kalman/KalmanFilter.cpp
+++ src/kalman/KalmanFilter.cpp
@@ -15,6 +15,11 @@ inline static void mat_init(arm_matrix_instance_f32* mat, uint16_t rows, uint16_
 	arm_mat_init_f32(mat, rows, cols, (float*)calloc((rows * cols), sizeof(float)));
 }
 
+inline static void mat_destroy(arm_matrix_instance_f32 *mat) {
+    free(*mat.pData);
+    &*mat = NULL;//TODO is this how to destroy the matrix?
+}
+
 /**
  * @brief Returns a reference to the matrix at the specified row and column
  *
@@ -31,7 +36,6 @@ inline static float& mat_at(arm_matrix_instance_f32& mat, uint16_t row, uint16_t
 
 inline static void mat_fill(arm_matrix_instance_f32& mat, float toFill)
 {
-    //TODO should this be uint16
 	for(uint16_t i = 0; i < mat.numRows * mat.numCols; i++)
     {
 		mat.pData[i] = toFill;
@@ -45,10 +49,16 @@ KalmanFilter::KalmanFilter() :
 	p_size(2),
 	c_size(3) 
 {
+    //initialize state matrix and set values to 0
 	mat_init(&state, n_size, 1);
 	mat_fill(state, 0);
+
+    //initialize covariances matrix and set them to 0 too
 	mat_init(&P, n_size, n_size);
 	mat_fill(P, 0);
+
+    //A = diag(1, 1, 1, 1, 1, 1)
+    //note that A will have changes made to it within some of the functions
 	mat_init(&A, n_size, n_size);
 	mat_fill(A, 0);
 	mat_at(A, 0, 0) = 1;
@@ -57,105 +67,117 @@ KalmanFilter::KalmanFilter() :
 	mat_at(A, 3, 3) = 1;
 	mat_at(A, 4, 4) = 1;
 	mat_at(A, 5, 5) = 1;
+
+    //initialize B and set values to 0
 	mat_init(&B, n_size, u_size);
 	mat_fill(B, 0);
+
+    //initialize and zero Q
 	mat_init(&Q, n_size, n_size);
 	mat_fill(Q, 0);
+
+    //initialize and zero R for the lidar
 	mat_init(&R_lidar, l_size, l_size);
 	mat_fill(R_lidar, 0);
+
+    //initialize and zero R for the Px4
 	mat_init(&R_Px4, p_size, p_size);
 	mat_fill(R_Px4, 0);
+
+    //initialize and zero R for the camera
 	mat_init(&R_camera, c_size, c_size);
 	mat_fill(R_camera, 0);
+
+    //H_lidar = [ 0 0 0 0 1 0; 0 0 0 0 0 1 ]
 	mat_init(&H_lidar, l_size, n_size);
 	mat_fill(H_lidar, 0);
 	mat_at(H_lidar, 0, 4) = 1;
 	mat_at(H_lidar, 1, 5) = 1;
+
+    //H_Px4 = [ 0 1 0 0 0 0; 0 0 0 1 0 0 ]
 	mat_init(&H_Px4, p_size, n_size);
 	mat_fill(H_Px4, 0);
 	mat_at(H_lidar, 0, 1) = 1;
 	mat_at(H_lidar, 0, 4) = 1;
+
+    //H_camera = [ 1 0 0 0 0 0; 0 0 1 0 0 0 ] TODO correct?
 	mat_init(&H_camera, c_size, n_size);
 	mat_fill(H_camera, 0);
+    mat_at(H_camera, 0, 0) = 1;
+    mat_at(H_camera, 1, 2) = 1;
+
+    //initialize z matrices
     mat_init(&z_2by1, 2, 1);
     mat_init(&z_3by1, 3, 1);
+
+    //initialize a bunch of intermediate matrices TODO no need to bother filling these, right?
 	mat_init(&inter_nby1, n_size, 1); //used as temporary variables
-	mat_fill(inter_nby1, 0);
 	mat_init(&inter_nbyn, n_size, n_size);
-	mat_fill(inter_nbyn, 0);
 	mat_init(&inter_another_nbyn, n_size, n_size);
-	mat_fill(inter_another_nbyn, 0);
 	mat_init(&inter_2by1, 2, 1);
-	mat_fill(inter_2by1, 0);
 	mat_init(&inter_nby2, n_size, 2);
-	mat_fill(inter_nby2, 0);
 	mat_init(&inter_another_nby2, n_size, 2);
-	mat_fill(inter_another_nby2, 0);
 	mat_init(&inter_2byn, 2, n_size);
-	mat_fill(inter_2byn, 0);
 	mat_init(&inter_2by2, 2, 2);
-	mat_fill(inter_2by2, 0);
 	mat_init(&inter_another_2by2, 2, 2);
-	mat_fill(inter_another_2by2, 0);
 }
 
 KalmanFilter::~KalmanFilter()
 {
-	free(state.pData);
-	free(P.pData);
-	free(A.pData);
-	free(B.pData);
-	free(Q.pData);
-	free(R_lidar.pData);
-	free(R_Px4.pData);
-	free(R_camera.pData);
-	free(H_lidar.pData);
-	free(H_Px4.pData);
-	free(H_camera.pData);
-    free(z_2by1.pData);
-    free(z_3by1.pData);
-	free(inter_nby1.pData);
-	free(inter_nbyn.pData);
-	free(inter_another_nbyn.pData);
-	free(inter_2by1.pData);
-	free(inter_nby2.pData);
-	free(inter_another_nby2.pData);
-	free(inter_2byn.pData);
-	free(inter_2by2.pData);
-	free(inter_another_2by2.pData);
+	mat_destroy(state);
+	mat_destroy(P);
+	mat_destroy(A);
+	mat_destroy(B);
+	mat_destroy(Q);
+	mat_destroy(R_lidar);
+	mat_destroy(R_Px4);
+	mat_destroy(R_camera);
+	mat_destroy(H_lidar);
+	mat_destroy(H_Px4);
+	mat_destroy(H_camera);
+    mat_destroy(z_2by1);
+    mat_destroy(z_3by1);
+	mat_destroy(inter_nby1);
+	mat_destroy(inter_nbyn);
+	mat_destroy(inter_another_nbyn);
+	mat_destroy(inter_2by1);
+	mat_destroy(inter_nby2);
+	mat_destroy(inter_another_nby2);
+	mat_destroy(inter_2byn);
+	mat_destroy(inter_2by2);
+	mat_destroy(inter_another_2by2);
 }
 
-void KalmanFilter::predict(const arm_matrix_instance_f32 u, float delta_t)
+void KalmanFilter::predict(const arm_matrix_instance_f32 &u, float dt)
 {
-	//Put delta_t in the appropriate spot in A and B
+	//Put dt in the appropriate spot in A and B
 	//note: right now this assumes A is 6x6 and B is 6x3
-	mat_at(A, 0, 1) = delta_t;
-	mat_at(A, 2, 3) = delta_t;
-	mat_at(A, 4, 5) = delta_t;
-	mat_at(B, 1, 0) = delta_t;
-	mat_at(B, 3, 1) = delta_t;
-	mat_at(B, 5, 2) = delta_t;
+	mat_at(A, 0, 1) = dt;
+	mat_at(A, 2, 3) = dt;
+	mat_at(A, 4, 5) = dt;
+	mat_at(B, 1, 0) = dt;
+	mat_at(B, 3, 1) = dt;
+	mat_at(B, 5, 2) = dt;
 
 	//predict step for state
-	arm_mat_mult_f32(&A, &state, &inter_nby1);
-	state = inter_nby1;
-	arm_mat_mult_f32(&B, &u, &inter_nby1);
-	arm_mat_add_f32(&state, &inter_nby1, &state);
+	arm_mat_mult_f32(&A, &state, &inter_nby1); // A * x
+	state = inter_nby1; // x = A * x
+	arm_mat_mult_f32(&B, &u, &inter_nby1); //B * u
+	arm_mat_add_f32(&state, &inter_nby1, &state);//x = A * x + B * u
 
 	//predict step for P
-	arm_mat_mult_f32(&A, &P, &inter_nbyn);
-	arm_mat_trans_f32(&A, &inter_another_nbyn);
-	arm_mat_mult_f32(&inter_nbyn, &inter_another_nbyn, &P);
-	arm_mat_add_f32(&P, &Q, &P);
+	arm_mat_mult_f32(&A, &P, &inter_nbyn); //A * P
+	arm_mat_trans_f32(&A, &inter_another_nbyn); //A^T
+	arm_mat_mult_f32(&inter_nbyn, &inter_another_nbyn, &P); //P = A * P * A^T
+	arm_mat_add_f32(&P, &Q, &P); //P = A * P * A^T + Q
 }
 
-//if Px4 is true, we're correcting the Px4. Otherwise we're correcting the lidar
 void KalmanFilter::correct2(const arm_matrix_instance_f32& z, const bool Px4)
 {
 
 	arm_matrix_instance_f32* H;
 	arm_matrix_instance_f32* R;
-	if(Px4)
+	if( Px4 )//if Px4 is true, we're correcting with the Px4, if not, with lidar
 	{
 		H = &H_Px4;
 		R = &R_Px4;
@@ -208,6 +230,7 @@ void KalmanFilter::correct2(const arm_matrix_instance_f32& z, const bool Px4)
 
 void KalmanFilter::correctPx4(const float xdot, const float ydot)
 {
+    //z_2by1 = [ xdot; ydot ]
     mat_at(z_2by1, 0, 0) = xdot;
     mat_at(z_2by1, 1, 0) = ydot;
 	correct2(z_2by1, true);
@@ -215,14 +238,17 @@ void KalmanFilter::correctPx4(const float xdot, const float ydot)
 
 void KalmanFilter::correctLidar(const float z, const float zdot)
 {
+    //z_2by1 = [ z; zdot ]
     mat_at(z_2by1, 0, 0) = z;
     mat_at(z_2by1, 0, 1) = zdot;
 	correct2(z_2by1, false);
 }
 
+//TODO figure out how to write this, specifically: where does yaw go? Is it even
+//part of the state matrix? What are we supposed to do with it?
 void KalmanFilter::correctCamera(const float x, const float y, const float yaw)
 {
-
+    //z_3by1 = [ x, y ]
 }
 
 const arm_matrix_instance_f32& KalmanFilter::getState() const {
