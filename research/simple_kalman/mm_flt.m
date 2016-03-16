b846cbb81f36fbc6b1fb3235a8c67c798fd37ffd
Nicholas Eckardt <neckardt@umich.edu>
2016-03-15 22:08:32 -0400

    worked on filtering px4 stuff

--- research/simple_kalman/mm_flt.m
+++ research/simple_kalman/mm_flt.m
@@ -2,7 +2,9 @@ clear all;
 clc;
 close all;
 
-log = load('../Log_analysis/23feb2016/LOG94.TXT');
+log = load('../Log_analysis/2016/q1/23feb2016/LOG94.TXT');
+
+%log = load('RunFilterTestLog.TXT');
 Time          = log(:,1);
 Imu_AccX      = log(:,3);
 Imu_AccY      = log(:,4);
@@ -25,6 +27,14 @@ mm_size = 31;
 mm_buf = zeros(3,1);
 mm_idx = 1;
 
+
+
+px4_filt = zeros(3,1);
+mm_size2 = 15;
+%THe third row is not used, just a placeholder of zero
+mm_buf2 = zeros(3,1);
+mm_idx2 = 1;
+
 for i = 1:log_length
     % rotation matrix that turns vectors in quadcopter frame to earth frame
     rotMat = reshape(Imu_Rot(i, :), [3, 3]);
@@ -33,6 +43,11 @@ for i = 1:log_length
     imu = [Imu_AccX(i), Imu_AccY(i), Imu_AccZ(i)]';
     imu = rotMat * imu;
     imu(3) = imu(3) + 9.81;
+    
+     % rotating px4
+    px4 = [Px4_Xdot(i), Px4_Ydot(i), 0]';
+    px4 = rotMat * px4;
+    
     
     %% MM FLT coded out similar to how it would be implemented in C++     
     mm_buf(:, mm_idx) = imu;
@@ -46,6 +61,19 @@ for i = 1:log_length
     imu_flt_hist(:,i) = imu_flt;
     imu_hist(:,i) = imu;
     Time_hist(:,i) = Time(i);
+    
+    
+    %% MM FLT coded out similar to how it would be implemented in C++     
+    mm_buf2(:, mm_idx2) = px4;
+    mm_idx2 = mm_idx2 + 1;
+    if (mm_idx2 > mm_size2)
+      mm_idx2 = 1;
+    end    
+    
+    px4_flt = mean(mm_buf2, 2);
+   
+    px4_flt_hist(:,i) = px4_flt;
+    px4_hist(:,i) = px4;
 end
 
 figure(1)
@@ -67,7 +95,25 @@ xlabel('Time (s)');
 title('AccZ Flt (red) & AccZ Raw (blk)');
 
 
+
+figure(4)
+plot(Time_hist, px4_hist(1,:), '*k', Time_hist, px4_flt_hist(1,:), 'r');
+ylabel('X Dot (m/s)');
+xlabel('Time (s)');
+title('XDot Flt (red) & X Raw (blk)');
+
+figure(5)
+plot(Time_hist, px4_hist(2, :), '*k', Time_hist, px4_flt_hist(2,:), 'r');
+ylabel('Y Dot (m/s)');
+xlabel('Time (s)');
+title('YDot Flt (red) & Y Raw (blk)');
+
+figure(6)
+plot(Time_hist, Px4_Qual, '*k', Time_hist, Lidar_Dist/2, 'r');
+title('Qual');
+
 %% Moving avg
+%{
 b = 1/31 * ones(1, 31);
 a = 1;
 mm_flt = filter(b, a, imu_hist(3,:));
@@ -78,3 +124,4 @@ plot(Time_hist, imu_hist(3,:), '*k', Time_hist, mm_flt, 'r');
 
 figure(5)
 plot(Time_hist, mm_flt, '*k', Time_hist, imu_flt_hist(3,:), 'r');
+%}
