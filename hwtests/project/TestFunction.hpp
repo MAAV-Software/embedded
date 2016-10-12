1c8690cadccdc251a37cd8560c2d91f8bb672a20
Izumi Konata <sasawat@outlook.com>
2016-10-12 00:52:29 -0400

    Structure for TestFunction set up

---
+++ hwtests/project/TestFunction.hpp
@@ -0,0 +1,23 @@
+/*
+ * TestFunction.hpp
+ *
+ * Header for Test Function Class
+ *
+ * Please Implement the Test Function Class in each Unit Test
+ *
+ *      Author: Sasawat Prankprakma
+ *        Date: Oct 12, 2016
+ */
+
+#ifndef TESTFUNCTION_HPP_
+#define TESTFUNCTION_HPP_
+
+
+class TestFunction
+{
+public:
+    void run();
+};
+
+
+#endif /* TESTFUNCTION_HPP_ */
