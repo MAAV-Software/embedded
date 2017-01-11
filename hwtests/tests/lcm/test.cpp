c66667f3f2ffbf17bddf1d4adfef7b2a30d98e62
Sasawat Prankprakma <psasawat@umich.edu>
2017-01-10 21:24:20 -0500

    LCM Test Case. Also moving away from CoverageManifest System

---
+++ hwtests/tests/lcm/test.cpp
@@ -0,0 +1,62 @@
+#include "TestFunction.hpp"
+#include <stdint.h>
+#include "driverlib/sysctl.h"
+#include "LED.h"
+#include "time_util.h"
+
+#include "ProgramState.hpp"
+#include "messaging/DataLink.hpp"
+#include "DataLinkHw.hpp"
+#include "LED.h"
+#include "time_util.h"
+#include "EEPROM.h"
+
+#include "messaging/RingBuffer.hpp"
+#include "messaging/emergency_t.h"
+#include "messaging/feedback_t.h"
+#include "messaging/gains_t.h"
+#include "messaging/raw_pose_t.h"
+#include "messaging/setpt_t.h"
+
+
+void TestFunction::run()
+{
+	Config_LED();
+	Config_EEPROM();
+	time_init(SYSCTL_PERIPH_TIMER1, SYSCLOCK, TIMER1_BASE, INT_TIMER1A);
+
+	//Set up a DataLink Object
+	DataLink dl(DataLinkUartSend);
+
+	//Configure UART
+	// THIS IS FOR BOTH 2015 AND 2016 SIGNAL BOARDS
+	DataLinkUartConfig(SYSCTL_PERIPH_UART0, SYSCTL_PERIPH_GPIOA, GPIO_PA0_U0RX,
+					   GPIO_PA1_U0TX, GPIO_PORTA_BASE, GPIO_PIN_0, GPIO_PIN_1,
+					   UART0_BASE, INT_UART0);
+
+	int32_t lastSetptTime = 0;
+	setpt_t setpt;
+
+	for(;;)
+	{
+		// Process incoming buffers
+		while (DL_RBUFF.unread() > 0) // send any received bytes to DataLink for parsing
+			dl.processRecv(DL_RBUFF.pop());
+
+		// grab current message
+		setpt = dl.getSetptMsg();
+
+		// if we got a new message, blink the LED
+		// new message is determined by INCREASE in timestamp
+		if (setpt.utime > lastSetptTime)
+		{
+			// copy the new time
+		    lastSetptTime = setpt.utime;
+		    // blink the LED
+		    Toggle_LED(BLUE_LED, SYSCLOCK / 1000);
+		}
+
+	}
+
+
+}
